53b59fb573343f040e04957e9955060d2bac0853
kannanhassouna <knanahassouna2@gmail.com>
2020-05-14 01:52:39 +0300

    first commit

---
+++ ios/AppCenter-Config.h
@@ -0,0 +1,16 @@
+//
+//  AppCenter-Config.h
+//  firstApp
+//
+//  Created by Kanna'n Hassouna on 5/14/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppCenter_Config : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/AppCenter-Config.m
@@ -0,0 +1,12 @@
+//
+//  AppCenter-Config.m
+//  firstApp
+//
+//  Created by Kanna'n Hassouna on 5/14/20.
+//
+
+#import "AppCenter-Config.h"
+
+@implementation AppCenter_Config
+
+@end
