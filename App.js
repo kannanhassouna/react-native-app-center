2da77bf68f6097084121fa8da36ac997950f575a
Kanna'n <kannan.m.hassouna@gmail.com>
2022-02-19 22:41:37 +0200

    testing git

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ import {
 const App: () => React$Node = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar barStyle="dark-1content" />
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
